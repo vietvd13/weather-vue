0c3553d9f37d10bdc94d7bec62c343ed7700c945
vietvd13 <vuducviet0131@gmail.com>
2021-12-01 17:19:24 +0700

    Add project

---
+++ src/const/api.js
@@ -0,0 +1,4 @@
+export default {
+  api_key: "df24978067427fe992301ab2fe332253",
+  url_base: "https://api.openweathermap.org/data/2.5/",
+};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 Vue.config.productionTip = false;
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+import "./assets/font-awesome/css/all.min.css";
 
 new Vue({
   render: (h) => h(App),
